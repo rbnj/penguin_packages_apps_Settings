9e104f11856d54a504a5139c23d1faac5cabbab1
idoybh <idoybh2@gmail.com>
2024-09-21 12:17:12 +0530

    Settings: Allow choosing whether to show % inside battery icon [2/2]

--- src/com/penguin/settings/laboratory/LabSettings.java
+++ src/com/penguin/settings/laboratory/LabSettings.java
@@ -22,6 +22,7 @@ import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -30,14 +31,19 @@ import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
+
+import co.aospa.framework.preference.SecureSettingListPreference;
+import co.aospa.framework.preference.SystemSettingSwitchPreference;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
@@ -47,20 +53,70 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-@SearchIndexable public class LabSettings extends SettingsPreferenceFragment
-implements
-        Preference.OnPreferenceChangeListener, Indexable { @Override public void
-    onCreate(Bundle icicle) {
-        super.onCreate(icicle); ContentResolver resolver =
-        getActivity().getContentResolver();
-        addPreferencesFromResource(R.xml.penguin_lab_settings); final Resources res
-        = getResources();
+@SearchIndexable 
+public class LabSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    // Battery options
+    private static final String BATTERY_STYLE = "status_bar_battery_style";
+    private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String SHOW_BATTERY_PERCENT_INSIDE = "status_bar_show_battery_percent_inside";
+
+    private SecureSettingListPreference mBatteryStyle;
+    private SystemSettingSwitchPreference mBatteryPercent;
+    private SystemSettingSwitchPreference mBatteryPercentInside;
+
+    @Override 
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle); 
+        addPreferencesFromResource(R.xml.penguin_lab_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mBatteryPercentInside = (SystemSettingSwitchPreference)
+                findPreference(SHOW_BATTERY_PERCENT_INSIDE);
+        mBatteryPercent = (SystemSettingSwitchPreference)
+                findPreference(SHOW_BATTERY_PERCENT);
+        boolean enabled = Settings.System.getIntForUser(resolver,
+                SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
+        mBatteryPercent.setChecked(enabled);
+        mBatteryPercent.setOnPreferenceChangeListener(this);
+        mBatteryPercentInside.setEnabled(enabled);
+
+        mBatteryStyle = (SecureSettingListPreference)
+                findPreference(BATTERY_STYLE);
+        int value = Settings.System.getIntForUser(resolver,
+                BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
+        mBatteryStyle.setValue(Integer.toString(value));
+        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+        updatePercentEnablement(value != 2);
     }
 
-    @Override public boolean onPreferenceChange(Preference preference, Object
-    newValue) {
-        ContentResolver resolver = getActivity().getContentResolver(); return
-        false;
+    @Override 
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryStyle) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mBatteryStyle.findIndexOfValue((String) objValue);
+            mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
+            Settings.System.putIntForUser(resolver,
+                    BATTERY_STYLE, value, UserHandle.USER_CURRENT);
+            updatePercentEnablement(value != 2);
+            return true;
+        } else if (preference == mBatteryPercent) {
+            boolean enabled = (boolean) objValue;
+            Settings.System.putInt(resolver,
+                    SHOW_BATTERY_PERCENT, enabled ? 1 : 0);
+            mBatteryPercentInside.setEnabled(enabled);
+            return true;
+        }
+        return false;
+    }
+
+    private void updatePercentEnablement(boolean enabled) {
+        mBatteryPercent.setEnabled(enabled);
+        mBatteryPercentInside.setEnabled(enabled && mBatteryPercent.isChecked());
     }
 
     @Override public void onResume() { super.onResume();
@@ -77,24 +133,6 @@ implements
 
     }
 
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                                                                            boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.penguin_lab_settings;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-                }
-            };
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.penguin_lab_settings);
 }
